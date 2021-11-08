637f4145430e4a68fefee442a5df566b28f5edd5
ErinM663 <90435702+ErinM663@users.noreply.github.com>
2021-11-07 23:09:29 -0500

    Update Project3.js

--- Project3.js
+++ Project3.js
@@ -1,50 +1,56 @@
-/* const questions = [
-  {
-    questionType : "true_false",
-    questionText : "The earth is round",
-    correctAnswer : "true",
-    options : ["true", "false"],
-  },
-  {
-    questionType : "text_input",
-    questionText : "What is the value of the expression 1+1",
-    correctAnswer : "2",
-    answerFieldId : "answer_to_question" */
-  }
-]
-async function fetch_Questions_async() {
-  const response = await fetch('my-json-server.typicode.com/ErinM663/Repository');
-    const result = await response.json(); 
-     updateDOM(result);
-}
-function updateDOM(result){
-let question = result.appState.current_question[0];
-  let HTMLString = `<br> Question: ${question}`;
-  document.querySelector("#widget_view").innerHTML = HTMLString;
-}
-
-
-const appState = {
+let questions =20;
+let appState = {
     current_view : "#intro_view",
     current_question : -1,
+    current_quiz: "",
     current_model : {},
     current_correct:0,
     current_wrong:0,
     current_percentage:0
+
 }
 
-//
-// start_app: begin the applications.
-//
+
+async function getQuestions(questionId, quizSelection) {
+  let api_url = "";
+
+  if (quizSelection == "quiz1") {
+    api_url = 'https://my-json-server.typicode.com/ErinM663/repository/'
+  }
+  else if (quizSelection == "quiz2") {
+    api_endpoint_url = 'https://my-json-server.typicode.com/ErinM663/QUIZ1/'
+  }
+
+  let api_end = `${api_url}/${quizSelection}/${questionId}`
+  const response = await fetch(api_end);
+  const result = await response.json();
+
+  appState.current_model = result;
+
+  setQuestionView(appState);
+  update_view(appState);
+
+  document.getElementById("AnsweredQuestions").innerHTML = appState.current_correct + appState.current_wrong;
+  if (questionId == 1) {
+    document.getElementById("currentScore").innerHTML = 0;
+  }
+  else {
+    document.getElementById("currentScore").innerHTML = +(((appState.current_correct / (appState.current_correct + appState.current_wrong)) * 100).toFixed(2));
+  appState.current_percentage=(((appState.current_correct / (appState.current_correct + appState.current_wrong)) * 100));
+  }
+
+
+  return (result);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   // Set the state
   appState.current_view =  "#intro_view";
   appState.current_model = {
-    action : "start_app"
+    action : "quiz1",
+    action2: "quiz2"
   }
   update_view(appState);
-
   //
   // EventDelegation - handle all events of the widget
   //
@@ -55,22 +61,48 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+let minutesLabel = "";
+let secondsLabel = "";
+let timer = 0;
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+
+}
+
+
+
+
+
 
 function handle_widget_event(e) {
 
   if (appState.current_view == "#intro_view"){
-    if (e.target.dataset.action == "start_app") {
-        fetch_Questions_async()
-        // Update State (current model + state variables)
-        appState.current_question = 0
-        appState.current_model = questions[appState.current_question];
-        // process the appState, based on question type update appState.current_view
-        setQuestionView(appState);
+        timer = setInterval(setTime, 1000);
+         minutesLabel = document.getElementById("minutes");
+         secondsLabel = document.getElementById("seconds");
+    if (e.target.dataset.action == "quiz1") {
+        appState.current_quiz ="quiz1";
+        appState.current_question=0;
+        getQuestions(appState.current_question + 1, appState.current_quiz)
 
-        // Now that the state is updated, update the view.
 
-        update_view(appState);
-    }
+       }
+      else if (e.target.dataset.action == "quiz2") {
+
+         appState.current_quiz ="quiz2";
+         appState.current_question=0;
+         getQuestions(appState.current_question + 1, appState.current_quiz)
+               }
   }
 
   // Handle the answer event.
@@ -79,52 +111,143 @@ function handle_widget_event(e) {
     if (e.target.dataset.action == "answer") {
        // Controller - implement logic.
        isCorrect = check_user_response(e.target.dataset.answer, appState.current_model);
+        if (isCorrect) {
+                appState.current_correct++;
+              }
+              else {
+                appState.current_wrong++;
+              }
+
 
        // Update the state.
-       appState.current_question =   appState.current_question + 1;
-       appState.current_model = questions[appState.current_question];
+       updateQuestion(appState);
        setQuestionView(appState);
-
+        setFeedback(isCorrect);
        // Update the view.
        update_view(appState);
+       }
 
-     }
    }
 
    // Handle answer event for  text questions.
    if (appState.current_view == "#question_view_text_input") {
-     
-     if (e.target.dataset.action == "submit") {
-
-           user_response = document.querySelector(`#${appState.current_model.answerFieldId}`).value;
-           isCorrect = check_user_response(e.target.dataset.answer, appState.current_model);
+       if (e.target.dataset.action == "submit") {
+       user_response = document.querySelector(`#${appState.current_model.answerFieldId}`).value;
+          isCorrect = check_user_response(e.target.dataset.answer, appState.current_model);
+           if (isCorrect) {
+           appState.current_correct++;
+           }
+           else {
+             appState.current_wrong++;
+           }
+            setFeedback(isCorrect);
            updateQuestion(appState)
-           //appState.current_question =   appState.current_question + 1;
-           //appState.current_model = questions[appState.current_question];
            setQuestionView(appState);
            update_view(appState);
        }
     }
+    if (appState.current_view == "#question_view_multiple") {
 
-    // Handle answer event for  text questions.
-    if (appState.current_view == "#end_view") {
-        if (e.target.dataset.action == "start_again") {
-          appState.current_view =  "#intro_view";
-          appState.current_model = {
-            action : "start_app"
-          }
-          update_view(appState);
+        if (e.target.dataset.action == "submit") {
+             let selection = document.getElementsByName("selection");
+             let user_response;
 
+             for (let i = 0; i < choices.length; i++) {
+               if (selection[i].checked) {
+                 user_response = selection[i].value;
+               }
+             }
+
+             isCorrect = check_user_response(user_response, appState.current_model);
+             if (isCorrect) {
+               appState.current_correct++;
+             }
+             else {
+               appState.current_wrong++;
+             }
+
+             setFeedback(isCorrect);
+             update_view(appState);
+             updateQuestion(appState);
+             setQuestionView(appState);
+
+           }
+         }
+if (appState.current_view == "#question_view_check") {
+    if (e.target.dataset.action == "submit") {
+      var check = document.getElementsByName("box");
+      var checkChecked = [];
+
+      for (var i = 0; i < check.length; i++) {
+        if (check[i].checked) {
+          checkChecked.push(check[i].value);
         }
       }
+
+      isCorrect = check_user_response(checkChecked, appState.current_model);
+      if (isCorrect) {
+        appState.current_correct++;
+      }
+      else {
+        appState.current_wrong++;
+      }
+
+      setFeedback(isCorrect);
+      update_view(appState);
+       updateQuestion(appState);
+      setQuestionView(appState);
+    }
+  }
+  if (appState.current_view == "#question_view_multi_input") {
+      if (e.target.dataset.action == "submit") {
+        user_response1 = document.querySelector(`#${appState.current_model.answerField1}`).value;
+        user_response2 = document.querySelector(`#${appState.current_model.answerField2}`).value;
+
+        let textResponses = [];
+        textResponses.push(user_response1);
+        textResponses.push(user_response2);
+
+        isCorrect = check_user_response(textResponses, appState.current_model);
+        if (isCorrect) {
+          appState.current_correct++;
+        }
+        else {
+          appState.current_wrong++;
+        }
+
+        setFeedback(isCorrect);
+        update_view(appState);
+        updateQuestion(appState);
+        setQuestionView(appState);
+      }
+    }
+    if (appState.current_view == "#end_view") {
+        clearInterval (timer);
+        if (e.target.dataset.action == "start_again") {
+              appState.current_view = "#intro_view";
+              appState.current_question = -1,
+                appState.current_quiz = "",
+                appState.current_model = {},
+                appState.current_correct = 0,
+                appState.current_wrong = 0
+              appState.current_model = {
+                action: "quiz1",
+                action2: "quiz2"
+              }
+              update_view(appState);
+            }
+
+        }
+
 
  } // end of handle_widget_event
 
 
+
+
 function check_user_response(user_answer, model) {
   if (user_answer == model.correctAnswer) {
-  appState.current_correct +=1;
-  //appState.current_percentage= appState.current_correct/appState.cur;
+   appState.current_correct +=1
     return true;
   }
   appState.current_wrong +=1
@@ -133,9 +256,11 @@ function check_user_response(user_answer, model) {
 }
 
 function updateQuestion(appState) {
-    if (appState.current_question < questions.length-1) {
+    if (appState.current_question < (questions-1)) {
+
       appState.current_question =   appState.current_question + 1;
-      appState.current_model = questions[appState.current_question];
+      getQuestions(appState.current_question + 1, appState.current_quiz);
+
     }
     else {
       appState.current_question = -2;
@@ -149,13 +274,31 @@ function setQuestionView(appState) {
     return
   }
 
-  if (appState.current_model.questionType == "true_false")
-    appState.current_view = "#question_view_true_false";
+  if (appState.current_model.questionType == "true_false") {
+    appState.current_view = "#question_view_true_false";}
   else if (appState.current_model.questionType == "text_input") {
     appState.current_view = "#question_view_text_input";
   }
-}
+  else if (appState.current_model.questionType == "multiple") {
+      appState.current_view = "#question_view_multiple";
+  }else if (appState.current_model.questionType == "multi_input") {
+           appState.current_view = "#question_view_multi_input";
+     }
+  else if (appState.current_model.questionType == "checkBox") {
+             appState.current_view = "#question_view_check";
+       }
 
+
+
+
+}
+function setFeedback(isCorrect) {
+  if (isCorrect == true) {
+    appState.current_view = "#feedback_positive";
+  } else {
+    appState.current_view = "#feedback_negative";
+  }
+}
 function update_view(appState) {
 
   const html_element = render_widget(appState.current_model, appState.current_view)
@@ -164,13 +307,11 @@ function update_view(appState) {
 //
 
 const render_widget = (model,view) => {
-  // Get the template HTML
-  template_source = document.querySelector(view).innerHTML
-  // Handlebars compiles the above source into a template
-  var template = Handlebars.compile(template_source);
+   template_source = document.querySelector(view).innerHTML
+    // Handlebars compiles the above source into a template
+    var template = Handlebars.compile(template_source);
 
-  // apply the model to the template.
-  var html_widget_element = template({...model,...appState})
+    // apply the model to the template.
+    var html_widget_element = template({...model,...appState})
 
   return html_widget_element
-}
